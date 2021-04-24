2f74dcc844cef484d5de3a237386bf5fff714a55
Sergei Kutanov <sergeikutanov@gmail.com>
2021-04-24 15:27:58 -0700

    Update SortedArrayToBST.java

--- src/com/sergeik/trees/SortedArrayToBST.java
+++ src/com/sergeik/trees/SortedArrayToBST.java
@@ -1,11 +1,25 @@
 package com.sergeik.trees;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class SortedArrayToBST {
 
     public static void main(String[] args) {
         int[] nums = new int[]{-10, -3, 0, 5, 9};
         TreeNode rootResult = recSolution(nums);
+        List<Integer> resultArr = new LinkedList<>();
+        preOrder(rootResult, resultArr);
+        assert resultArr.equals(Arrays.asList(0, -10, -3, 5, 9));
+    }
 
+    private static void preOrder(TreeNode node, List<Integer> arr) {
+        if (node == null)
+            return;
+        arr.add(node.val);
+        preOrder(node.left, arr);
+        preOrder(node.right, arr);
     }
 
     private static TreeNode recSolution(int[] nums) {
