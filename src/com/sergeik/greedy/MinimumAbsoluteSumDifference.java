8443f6e7ee17c37a360decf1f3b4eeecf9ed6f67
sergei <sergei@malonetuning.com>
2021-05-21 10:56:53 -0700

    MinimumAbsoluteSumDifference

--- src/com/sergeik/greedy/MinimumAbsoluteSumDifference.java
+++ src/com/sergeik/greedy/MinimumAbsoluteSumDifference.java
@@ -2,9 +2,26 @@ package com.sergeik.greedy;
 
 import java.util.Arrays;
 
+/**
+ * You are given two positive integer arrays nums1 and nums2, both of length n.
+ *
+ * The absolute sum difference of arrays nums1 and nums2 is defined as the sum of |nums1[i] - nums2[i]|
+ * for each 0 <= i < n (0-indexed).
+ *
+ * You can replace at most one element of nums1 with any other element in nums1 to minimize the absolute sum difference.
+ *
+ * Return the minimum absolute sum difference after replacing at most one element in the array nums1.
+ * Since the answer may be large, return it modulo 109 + 7.
+ *
+ * |x| is defined as:
+ *
+ * x if x >= 0, or
+ * -x if x < 0.
+ */
 public class MinimumAbsoluteSumDifference {
 
     public static void main(String[] args) {
+
         assert 0 == solution(
                 new int[]{2,4,6,8,10},
                 new int[]{2,4,6,8,10}
@@ -20,29 +37,41 @@ public class MinimumAbsoluteSumDifference {
     }
 
     private static int solution(int[] nums1, int[] nums2) {
-        int[] sorted1 = nums1.clone();
-        Arrays.sort(sorted1);
-        int result = 0;
+        int modulo = 1000000007;
+        long sum = 0;
+
+        int[] sorted = Arrays.copyOf(nums1, nums1.length);
+        Arrays.sort(sorted);
+
         int save = 0;
         for (int i = 0; i < nums1.length; i++) {
-            result += Math.abs(nums1[i] - nums2[i]);
-            result %= 1000000007;
-        }
-        for (int i = 0; i < nums1.length; i++){
-            if (nums1[i] == nums2[i]) continue;  // already equal, diff is already minimized
-            int index = Arrays.binarySearch(sorted1, nums2[i]);
             int diff = Math.abs(nums1[i] - nums2[i]);
-            // nums2[i] is actually in nums1[i], so we can minimize the diff to 0!
-            if (index >= 0) {
+            sum += diff;
+            if (diff == 0)
+                continue;
+            int replIndex = Arrays.binarySearch(sorted, nums2[i]);
+            if (replIndex >= 0) {
                 save = Math.max(save, diff);
             } else {
-                // find the closest number in nums1 that is to nums2[i], need to look 2  numbers around the found index(supposed insertion index)
-                int actualIndex = -1* (index + 1);
-                if (actualIndex > 0) save = Math.max(save, diff - Math.abs(nums2[i] - sorted1[actualIndex-1]));
-                if (actualIndex < nums1.length) save = Math.max(save, diff - Math.abs(nums2[i] - sorted1[actualIndex]));
+                int actualIndex = -(replIndex + 1);
+                if (actualIndex > 0) {
+                    save = Math.max(
+                            save,
+                            diff - Math.abs(sorted[actualIndex - 1] - nums2[i])
+                    );
+                }
+                if (actualIndex < nums1.length) {
+                    save = Math.max(
+                            save,
+                            diff - Math.abs(sorted[actualIndex] - nums2[i])
+                    );
+                }
             }
         }
-        return (result - save) % 1000000007;
+
+        sum -= save;
+
+        return (int)(sum % modulo);
     }
 
 }
