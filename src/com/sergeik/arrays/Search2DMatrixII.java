e2562cd3d69119f6b0996e94ec9a213d277007a7
sergeikutanov <sergei@malonetuning.com>
2021-05-23 17:48:06 -0700

    Search2DMatrixII

--- src/com/sergeik/arrays/Search2DMatrixII.java
+++ src/com/sergeik/arrays/Search2DMatrixII.java
@@ -17,8 +17,8 @@ public class Search2DMatrixII {
                 {10,13,14,17,24},
                 {18,21,23,26,30}
         };
-        assert solution(matrix, 5);
-        assert !solution(matrix, 20);
+        assert dcSolution(matrix, 5);
+        assert !dcSolution(matrix, 20);
     }
 
     private static boolean solution(int[][] matrix, int target) {
@@ -36,6 +36,33 @@ public class Search2DMatrixII {
             }
         }
         return false;
+    }
+
+    private static boolean dcSolution(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return false;
+        return divideAndConquer(matrix, new int[]{0,0}, new int[]{matrix.length - 1, matrix[0].length - 1}, target);
+    }
+
+    private static boolean divideAndConquer(int[][] matrix, int[] leftTop, int[] bottomRight, int target) {
+        if (leftTop[0] > bottomRight[0] || leftTop[1] > bottomRight[1] || leftTop[0] >= matrix.length || bottomRight[1] > matrix[0].length)
+            return false;
+        if (bottomRight[0] - leftTop[0] == 0 && bottomRight[1] - leftTop[1] == 0) {
+            return matrix[leftTop[0]][leftTop[1]] == target;
+        }
+        int rowMid = (leftTop[1] + bottomRight[1]) << 1;
+        int colMid = (leftTop[0] + bottomRight[0]) << 1;
+        if (target > matrix[rowMid][colMid]) {
+            return divideAndConquer(matrix, leftTop, new int[]{rowMid, colMid}, target)
+                    || divideAndConquer(matrix, new int[]{leftTop[0], colMid+1}, new int[]{rowMid, bottomRight[1]}, target)
+                    || divideAndConquer(matrix, new int[]{rowMid+1, leftTop[1]}, new int[]{bottomRight[0], colMid}, target);
+        } else if (target < matrix[rowMid][colMid]) {
+            return divideAndConquer(matrix, leftTop, new int[]{rowMid, colMid}, target) ||
+                    divideAndConquer(matrix, new int[]{leftTop[0], colMid + 1}, new int[]{rowMid, bottomRight[1]}, target) ||
+                    divideAndConquer(matrix, new int[]{rowMid + 1, leftTop[1]}, new int[]{bottomRight[0], colMid}, target);
+        } else {
+            return true;
+        }
     }
 
 }
