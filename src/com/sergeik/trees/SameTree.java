dc14b0bfe11068bb7b42efe99daf7cd8325375b4
sergei <sergei@malonetuning.com>
2021-05-24 05:34:06 -0700

    SameTree

--- src/com/sergeik/trees/SameTree.java
+++ src/com/sergeik/trees/SameTree.java
@@ -1,5 +1,8 @@
 package com.sergeik.trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
  *
@@ -33,6 +36,29 @@ public class SameTree {
         if (p.val != q.val)
             return false;
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    private static boolean itSolution(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(p);
+        queue.offer(q);
+        while(!queue.isEmpty()) {
+            TreeNode n1 = queue.poll();
+            TreeNode n2 = queue.poll();
+            if (n1 == null ^ n2 == null)
+                return false;
+            if (n1 != null && n2 != null) {
+                if (n1.val != n2.val)
+                    return false;
+                queue.offer(n1.left);
+                queue.offer(n2.left);
+                queue.offer(n1.right);
+                queue.offer(n2.right);
+            }
+        }
+        return true;
     }
 
 
