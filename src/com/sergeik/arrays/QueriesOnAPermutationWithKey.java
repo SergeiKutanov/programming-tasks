7c0b728f5f1f34057044fc4e86103ab013ba87c3
Sergei Kutanov <sergeikutanov@gmail.com>
2021-07-18 20:52:02 -0700

    QueriesOnAPermutationWithKey

--- src/com/sergeik/arrays/QueriesOnAPermutationWithKey.java
+++ src/com/sergeik/arrays/QueriesOnAPermutationWithKey.java
@@ -1,9 +1,6 @@
 package com.sergeik.arrays;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Given the array queries of positive integers between 1 and m, you have to process all queries[i]
@@ -17,10 +14,58 @@ import java.util.List;
 public class QueriesOnAPermutationWithKey {
 
     public static void main(String[] args) {
-        assert Arrays.equals(new int[] {2,1,2,1}, solution(new int[]{3,1,2,1}, 5));
+        assert Arrays.equals(new int[] {2,1,2,1}, bitSolution(new int[]{3,1,2,1}, 5));
     }
 
-    private static int[] solution(int[] queries, int m) {
+    private static int[] bitSolution(int[] queries, int m) {
+        int n = queries.length;
+        BIT bit = new BIT(m + n + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 1; i <= m; i++) {
+            bit.add(n + i, 1);
+            map.put(i, n + i);
+        }
+
+        int[] res =new int[n];
+        for (int i = 0; i < n; i++) {
+            int index = map.remove(queries[i]);
+            res[i] = bit.prefixSum(index - 1);
+
+            int newIndex = n - i;
+            bit.add(index, -1);
+            bit.add(newIndex, 1);
+            map.put(queries[i], newIndex);
+        }
+        return res;
+    }
+
+    static class BIT {
+        int[] data;
+
+        BIT(int size) {
+            data = new int[size];
+        }
+
+        void add(int index, int delta) {
+            index++;
+            while (index < data.length) {
+                data[index] += delta;
+                index += index & (-index);
+            }
+        }
+
+        int prefixSum(int index) {
+            index++;
+            int res = 0;
+            while (index > 0) {
+                res += data[index];
+                index -= index & (-index);
+            }
+            return res;
+        }
+    }
+
+    private static int[] listSolution(int[] queries, int m) {
         List<Integer> list = new ArrayList<>();
         for (int i = 1; i <= m; i++)
             list.add(i);
